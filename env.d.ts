3e73462a8bfbf528f182c1c051f681f1460bc9a7
ranyunlong <ranyl@mininglamp.com>
2024-09-09 18:01:50 +0800

    feat: 改为输出umd目录,

--- env.d.ts
+++ env.d.ts
@@ -21,3 +21,16 @@
  */
 
 /// <reference types="@types/node" />
+declare module 'axios/lib/defaults' {
+  export default {};
+}
+
+declare module 'axios/lib/core/mergeConfig' {
+  import { AxiosRequestConfig } from 'axios';
+  function mergeConfig(
+    defaultConfig: AxiosRequestConfig,
+    instanceConfig?: AxiosRequestConfig
+  ): AxiosRequestConfig;
+
+  export default mergeConfig;
+}
